6d54d764da5018871c173fafb89b5cf0300756da
Isradanial <152113932+Isradanial@users.noreply.github.com>
2023-11-27 11:26:59 +0700

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,6 @@
+// FUNGSI PADA TOMBOL BAWAH KANAN UNTUK MENAMPILKAN ALERT
+const alertButton = document.getElementById("button");
+alertButton.addEventListener('click', function() {
+alert('Kirim ke whatsapp admin untuk menanyakan sesuatu');
+});
+
